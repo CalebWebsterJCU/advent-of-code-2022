557ada7eaf10d7a86b3fc7a7b1869b2093108d3e
caleb.webster@my.jcu.edu.au <calebwebsteredge@gmail.com>
2022-12-02 23:17:06 +1000

    Move code out of test file

--- spec/advent_of_code_2022/day1/core_spec.clj
+++ spec/advent_of_code_2022/day1/core_spec.clj
@@ -1,35 +1,6 @@
 (ns advent-of-code-2022.day1.core_spec
   (:require [speclj.core :refer :all]
-            [advent-of-code-2022.day1.core :refer :all]
-            [clojure.string :as str]))
-
-(defn parse-ints [coll] (map #(Integer/parseInt %) coll))
-
-(defn split-double-lines [str] (str/split str #"\r\n\r\n"))
-
-(defn parse-input [input-str]
-  (map parse-ints (map str/split-lines (split-double-lines input-str)))
-  )
-
-(defn get-bag-total [food-bag] (apply + food-bag))
-
-(defn get-bag-totals [food-bags] (map get-bag-total food-bags))
-
-(defn get-total-calories-of-top-elf [food-bags]
-  (apply max (get-bag-totals food-bags))
-  )
-
-(defn get-total-calories-of-top-three [food-bags]
-  (apply + (take 3 (sort > (get-bag-totals food-bags))))
-  )
-
-(defn solve-part-1 [input-str]
-  (get-total-calories-of-top-elf (parse-input input-str))
-  )
-
-(defn solve-part-2 [input-str]
-  (get-total-calories-of-top-three (parse-input input-str))
-  )
+            [advent-of-code-2022.day1.core :refer :all]))
 
 (def test-input "1000\r\n2000\r\n3000\r\n\r\n4000\r\n\r\n5000\r\n6000\r\n\r\n7000\r\n8000\r\n9000\r\n\r\n10000")
 
--- src/advent_of_code_2022/day1/core.clj
+++ src/advent_of_code_2022/day1/core.clj
@@ -1,5 +1,31 @@
-(ns advent-of-code-2022.day1.core)
+(ns advent-of-code-2022.day1.core
+  (:require [clojure.string :as str]))
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(defn parse-ints [coll] (map #(Integer/parseInt %) coll))
+
+(defn split-double-lines [str] (str/split str #"\r\n\r\n"))
+
+(defn parse-input [input-str]
+  (map parse-ints (map str/split-lines (split-double-lines input-str)))
+  )
+
+(defn get-bag-total [food-bag] (apply + food-bag))
+
+(defn get-bag-totals [food-bags] (map get-bag-total food-bags))
+
+(defn get-total-calories-of-top-elf [food-bags]
+  (apply max (get-bag-totals food-bags))
+  )
+
+(defn get-total-calories-of-top-three [food-bags]
+  (apply + (take 3 (sort > (get-bag-totals food-bags))))
+  )
+
+(defn solve-part-1 [input-str]
+  (get-total-calories-of-top-elf (parse-input input-str))
+  )
+
+(defn solve-part-2 [input-str]
+  (get-total-calories-of-top-three (parse-input input-str))
+  )
+
