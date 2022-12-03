6e71defc8231f30e64d4997b6a2aeae626e82ec5
caleb.webster@my.jcu.edu.au <calebwebsteredge@gmail.com>
2022-12-03 16:11:39 +1000

    Add day 2

--- spec/advent_of_code_2022/day2/core_spec.clj
+++ spec/advent_of_code_2022/day2/core_spec.clj
@@ -1,3 +1,31 @@
 (ns advent-of-code-2022.day2.core-spec
   (:require [speclj.core :refer :all]
             [advent-of-code-2022.day2.core :refer :all]))
+
+(def test-input "A Y\r\nB X\r\nC Z")
+
+(describe "Day 2 Part 1"
+          (it "Should parse input"
+              (should= [["A" "Y"] ["B" "X"] ["C" "Z"]] (parse-input test-input))
+              )
+          (it "Should return the correct result"
+              (should= :win (get-result-given-both-weapons "rock" "paper"))
+              (should= :lose (get-result-given-both-weapons "paper" "rock"))
+              (should= :draw (get-result-given-both-weapons "scissors" "scissors"))
+              )
+          (it "Should calculate the total score"
+              (should= 15 (solve-part-1 test-input))
+              )
+          (it "Should solve the puzzle"
+              (should= 14069 (solve-part-1 (slurp "spec/advent_of_code_2022/day2/input.txt")))
+              )
+          )
+
+(describe "Day 2 Part 2"
+          (it "Should calculate the total score"
+              (should= 12 (solve-part-2 test-input))
+              )
+          (it "Should solve the puzzle"
+              (should= 12411 (solve-part-2 (slurp "spec/advent_of_code_2022/day2/input.txt")))
+              )
+          )
--- src/advent_of_code_2022/day2/core.clj
+++ src/advent_of_code_2022/day2/core.clj
@@ -1,1 +1,77 @@
-(ns advent-of-code-2022.day2.core)
+(ns advent-of-code-2022.day2.core
+  (:require [clojure.string :as str]))
+
+(defn get-weapon-needed-to-achieve-result [opponents-weapon result]
+  (if (= :draw result)
+    opponents-weapon
+    (case opponents-weapon
+      "rock" (if (= :win result) "paper" "scissors")
+      "paper" (if (= :win result) "scissors" "rock")
+      "scissors" (if (= :win result) "rock" "paper")
+      )))
+
+(defn get-result-given-both-weapons [opponents-weapon your-weapon]
+  (if (= opponents-weapon your-weapon)
+    :draw
+    (case opponents-weapon
+      "rock" (if (= your-weapon "paper") :win :lose)
+      "paper" (if (= your-weapon "scissors") :win :lose)
+      "scissors" (if (= your-weapon "rock") :win :lose)
+      )))
+
+(def letter-to-weapon {
+                       "A" "rock"
+                       "B" "paper"
+                       "C" "scissors"
+                       "X" "rock"
+                       "Y" "paper"
+                       "Z" "scissors"
+                       })
+
+(def letter-to-result {
+                       "X" :lose
+                       "Y" :draw
+                       "Z" :win
+                       })
+
+(def weapon-scores {
+                    "rock"     1
+                    "paper"    2
+                    "scissors" 3
+                    })
+
+(def result-scores {
+                    :win  6
+                    :lose 0
+                    :draw 3
+                    })
+
+(defn split-by-space [string] (str/split string #" "))
+
+(defn parse-input [test-input]
+  (map split-by-space (str/split-lines test-input))
+  )
+
+(defn calc-round-score [your-weapon result]
+  (+ (get weapon-scores your-weapon) (get result-scores result))
+  )
+
+(defn get-round-score-for-part-1 [[letter1 letter2]]
+  (let [opponents-choice (get letter-to-weapon letter1)
+        your-choice (get letter-to-weapon letter2)
+        result (get-result-given-both-weapons opponents-choice your-choice)]
+    (calc-round-score your-choice result)
+    ))
+
+(defn get-round-score-for-part-2 [[letter1 letter2]]
+  (let [opponents-weapon (get letter-to-weapon letter1)
+        desired-result (get letter-to-result letter2)
+        your-weapon (get-weapon-needed-to-achieve-result opponents-weapon desired-result)]
+    (calc-round-score your-weapon desired-result)
+    ))
+
+(defn solve-part-1 [input-str]
+  (apply + (map get-round-score-for-part-1 (parse-input input-str))))
+
+(defn solve-part-2 [input-str]
+  (apply + (map get-round-score-for-part-2 (parse-input input-str))))
