1c92e539f3a768075407cb0c369c4a8ea5cc807d
caleb.webster@my.jcu.edu.au <calebwebsteredge@gmail.com>
2022-12-09 19:31:15 +1000

    Clean the code

--- spec/advent_of_code_2022/day1/core_spec.clj
+++ spec/advent_of_code_2022/day1/core_spec.clj
@@ -1,25 +1,24 @@
 (ns advent-of-code-2022.day1.core_spec
   (:require [speclj.core :refer :all]
-            [advent-of-code-2022.day1.core :refer :all]))
+            [advent-of-code-2022.day1.core :as core]))
 
-(def test-input "1000\r\n2000\r\n3000\r\n\r\n4000\r\n\r\n5000\r\n6000\r\n\r\n7000\r\n8000\r\n9000\r\n\r\n10000")
+(def example-input "1000\r\n2000\r\n3000\r\n\r\n4000\r\n\r\n5000\r\n6000\r\n\r\n7000\r\n8000\r\n9000\r\n\r\n10000")
+(def puzzle-input (slurp "spec/advent_of_code_2022/day1/input.txt"))
 
 (describe "Day 1 Part 1"
-          (it "Should parse input"
-              (should= [[1000 2000 3000] [4000] [5000 6000] [7000 8000 9000] [10000]] (parse-input test-input))
-              )
-          (it "Should find the max calories"
-              (should= 24000 (get-total-calories-of-top-elf (parse-input test-input)))
+          (it "Should solve the example"
+              (should= 24000 (core/solve-part-1 example-input))
               )
           (it "Should solve the puzzle"
-              (should= 71124 (solve-part-1 (slurp "spec/advent_of_code_2022/day1/input.txt"))))
+              (should= 71124 (core/solve-part-1 puzzle-input))
+              )
           )
 
 (describe "Day 1 Part 2"
-          (it "Should find the total calories of the top three elves"
-              (should= 45000 (get-total-calories-of-top-three (parse-input test-input)))
+          (it "Should solve the example"
+              (should= 45000 (core/solve-part-2 example-input))
               )
           (it "Should solve the puzzle"
-              (should= 204639 (solve-part-2 (slurp "spec/advent_of_code_2022/day1/input.txt")))
+              (should= 204639 (core/solve-part-2 puzzle-input))
               )
           )
--- src/advent_of_code_2022/day1/core.clj
+++ src/advent_of_code_2022/day1/core.clj
@@ -1,31 +1,37 @@
 (ns advent-of-code-2022.day1.core
   (:require [clojure.string :as str]))
 
-(defn parse-ints [coll] (map #(Integer/parseInt %) coll))
+(defn parse-calorie-value [snack-string] (Integer/parseInt snack-string))
 
-(defn split-double-lines [str] (str/split str #"\r\n\r\n"))
+(defn split-into-snacks [basket-string] (str/split-lines basket-string))
 
-(defn parse-input [input-str]
-  (map parse-ints (map str/split-lines (split-double-lines input-str)))
+(defn split-into-baskets [puzzle-input] (str/split puzzle-input #"\r\n\r\n"))
+
+(defn get-basket-total [basket] (apply + basket))
+
+(defn get-totals-of-all-baskets [baskets] (map get-basket-total baskets))
+
+(defn get-total-of-fullest-basket [baskets]
+  (apply max (get-totals-of-all-baskets baskets))
   )
 
-(defn get-bag-total [food-bag] (apply + food-bag))
-
-(defn get-bag-totals [food-bags] (map get-bag-total food-bags))
-
-(defn get-total-calories-of-top-elf [food-bags]
-  (apply max (get-bag-totals food-bags))
+(defn get-total-of-three-fullest-baskets [baskets]
+  (apply + (take 3 (sort > (get-totals-of-all-baskets baskets))))
   )
 
-(defn get-total-calories-of-top-three [food-bags]
-  (apply + (take 3 (sort > (get-bag-totals food-bags))))
+(defn parse-basket [basket-string]
+  (map parse-calorie-value (split-into-snacks basket-string))
   )
 
-(defn solve-part-1 [input-str]
-  (get-total-calories-of-top-elf (parse-input input-str))
+(defn parse-all-baskets [puzzle-input]
+  (map parse-basket (split-into-baskets puzzle-input))
   )
 
-(defn solve-part-2 [input-str]
-  (get-total-calories-of-top-three (parse-input input-str))
+(defn solve-part-1 [puzzle-input]
+  (get-total-of-fullest-basket (parse-all-baskets puzzle-input))
+  )
+
+(defn solve-part-2 [puzzle-input]
+  (get-total-of-three-fullest-baskets (parse-all-baskets puzzle-input))
   )
 
